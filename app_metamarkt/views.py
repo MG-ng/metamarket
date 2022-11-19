ce980cd2469c1afb6a5841ba4cd5211ee19f2d5f
Daniel Gockel <daniel@gockel.co>
2022-11-19 19:31:58 +0100

    search functionality

--- app_metamarkt/views.py
+++ app_metamarkt/views.py
@@ -1,11 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import generic
 
 
 def index(request):
-    if request.method == "POST":
-        pass
-    elif request.method == "GET":
-        return render(request, "app_metamarkt/index.html")
+    return render(request, "app_metamarkt/index.html")
+
+
+def search(request):
+    if request.method == "GET":
+        search_phrase = request.GET.get('q', None)
+        if search_phrase:
+            pass
+        return render(request, "app_metamarkt/search.html", {"search_phrase": search_phrase})
 
 
