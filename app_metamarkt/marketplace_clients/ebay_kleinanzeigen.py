4c329ee75d3104f93aeb577663c9a0d1801b6b00
Markus Gockel <markusgockel.mg@gmail.com>
2022-11-20 00:12:12 +0100

    Update Resultpage

---
+++ app_metamarkt/marketplace_clients/ebay_kleinanzeigen.py
@@ -0,0 +1,61 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+"""
+:return dict with minimal keys:
+    title
+    price
+    description
+"""
+def fetchData( query = "", location = "", radius = 0 ) -> list:
+    session = requests.Session()
+
+    domain = "https://www.ebay-kleinanzeigen.de/"
+    URL = domain + "s-" + query.replace( " ", "-" ) + "/k0"
+    print(f"{URL=}")
+
+    r = session.get( URL, headers = {
+        'referer': domain,
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7',
+    },
+                     timeout = 10,
+                     cookies = {
+                     } )
+    # print(f"{r.text=}")
+
+
+    site = BeautifulSoup( r.text, 'html.parser' )
+
+    results = []
+
+    for item in site.find_all( "li", {"class": "lazyload-item"} ):
+        # l = site.find_all( "li", {"class": "lazyload-item"} )
+        # print("l[0].html")
+        # print(l[0].html)
+        # print("l[0].text")
+        # print(l[0].text)
+        # print("l[0].getText")
+        # print(l[0].getText)
+        # print(f"{l[0].article.getText=}")
+
+        print()
+        print()
+        print(str(item.select("div")))
+
+        item_parsed = BeautifulSoup( str(item.article), 'html.parser' )
+
+        # parsed = BeautifulSoup( item.html, 'html.parser' )
+        results.append({
+            'title': item_parsed.select_one("ellipsis"),
+            'link': None
+        })
+
+
+    print(results)
+
+    return results
+
--- app_metamarkt/views.py
+++ app_metamarkt/views.py
@@ -12,44 +12,47 @@ def search(request):
     if request.method == "GET":
         search_phrase = request.GET.get('q', None)
         if search_phrase:
-            results = [{
-                        'title': 'Akkordeon Hohner Morino IV N',
-                        'description': '''Akkordeon wie neu mit Original-Koffer und gepolsterte Riemen
-                                        Grifftechnik Piano
-                                        Basstechnik Standardbass
-                                        Klangtechnik Cassotto
-                                        Stimmplattenqualitaet TAM
-                                        Stimmung 16' 8' 8' 4'
-                                        Farbe schwarz
-                                        Diskanttasten 41
-                                        Diskantchoere IV
-                                        Bassknoepfe 120
-                                        Basschoere V
-                                        Gewicht 11,9 kg
-                                        Register Diskant 11
-                                        Register Bass 5
-                                        Tonumfang F - a
-
-                                            Abmessungen 19 x 49 cm
-                                            
-                                            Da Privatverkauf keine Garantie und Rücknahme.''',
-                        'price': 2900,
+            results = [
+                {
+                        'title': 'BMW M4',
+                        'description': '''
+                        I'm selling this BMW because I drive it not that often. 
+                        ''',
+                        'price': 30000,
                         'priceUnit': 'EUR',
                         'zipCode': 80939,
                         'date': "08.10.2022",
-                        'views': 232
-            },
+                        'views': 232,
+                        'provider': "Mobile.de",
+                        'url': "https://suchen.mobile.de/fahrzeuge/details.html?id=356164356"
+                },
                 {
-                    'title': 'Akkordeon Hohner Concerto 1 rot, guter Zustand mit Koffer',
-                    'description': '''Dieses Akkordeon wurde leider sehr wenig gespielt und stand lange Zeit in der Wohnzimmer Ecke meiner Eltern. Es hat 26 Diskanttasten und 48 Bassknöpfe. Mit einem Gewicht von 4,7 Kilogramm ist es äußerst beliebt bei Anfängern und fortgeschrittenen Spielern. Bis auf eine leichte Delle (siehe Foto) hat es kaum Gebrauchsspuren, nichts ist ausgeleiert oder gar zerschlissen. Der Koffer hat deutlichere Gebrauchsspuren, lässt sich aber noch sauber öffnen und schließen (keine Schlüssel vorhanden). Interessenten sind herzlich eingeladen, das Instrument in Ruhe probezuspielen.
-In unserem Hauhalt gibt es keine Tiere, wir sind Nichtraucher. Verkauf erfolgt von privat, keine Garantie oder Rücknahme. Ich möchte das Instrument nicht verschicken, daher bitte ich um Abholung in Oberschleißheim. Und weil es viele Interessenten ignorieren wiederhole ich es nochmal: KEIN VERSAND - NUR ABHOLUNG''',
-                    'price': 299,
+                    'title': 'BMW M3',
+                    'description': '''
+                    This great BMW in Alpine white has never disappointed me.
+                    ''',
+                    'price': 66000,
                     'priceUnit': 'EUR',
                     'zipCode': 85764,
                     'date': "03.11.2022",
-                    'views': 115
+                    'views': 115,
+                    'provider': "ebay-kleinanzeigen",
+                    'url': "https://www.ebay-kleinanzeigen.de/s-anzeige/bmw-m3-f80-competition/2276986346-216-6479"
+                },
+                {
+                    'title': 'BMW M3',
+                    'description': '''
+                    This great BMW in Alpine white has never disappointed me.
+                    ''',
+                    'price': 66000,
+                    'priceUnit': 'EUR',
+                    'zipCode': 85764,
+                    'date': "03.11.2022",
+                    'views': 115,
+                    'provider': "ebay-kleinanzeigen",
+                    'url': "https://www.ebay-kleinanzeigen.de/s-anzeige/bmw-m3-f80-competition/2276986346-216-6479"
                 }]
-            results = quoka.fetch(search_phrase)
+            # results = quoka.fetch(search_phrase)
         else:
             results = None
         return render(request, "app_metamarkt/search.html", {"results": results})
