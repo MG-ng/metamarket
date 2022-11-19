3b9ee5dc15719302bcf87573952b22a390e78a18
Daniel Gockel <daniel@gockel.co>
2022-11-20 00:27:41 +0100

    adding quoka inserate

--- app_metamarkt/marketplace_clients/quoka.py
+++ app_metamarkt/marketplace_clients/quoka.py
@@ -1,3 +1,51 @@
+import requests
+from requests_html import HTMLSession
+from selenium import webdriver
+from seleniumrequests import Chrome
+from selenium.webdriver.common.by import By
+from parsel import Selector
+
+
+session = requests.Session()
 
 def fetch(search_phrase):
-    return search_phrase
+    ret = []
+
+    # webdriver = Chrome()
+    url = "https://www.quoka.de/alle-rubriken/kleinanzeigen.html"
+    payload = {
+        "search1": search_phrase
+    }
+    # response = webdriver.request('POST', url, data=payload)
+
+    # ret = response
+    # ret = webdriver.find_element(by=By.XPATH, value='//*[@id="ResultListData"]/ul/li')
+
+    response = requests.post(url, data=payload)
+    sel = Selector(response.text)
+    rows = sel.xpath('//*[@id="ResultListData"]/ul/li').getall()
+
+    for row in rows:
+        sel1 = Selector(row)
+        img = sel1.xpath('//li/div/a/img/@data-src').getall()
+        title = sel1.xpath('//li/div[2]/a//text()').getall()
+        description = sel1.xpath('//li/div[2]/div//text()').getall()
+        price = sel1.xpath('//li/div[3]/p[1]//text()').getall()
+        postalCode = sel1.xpath('//li/div[3]/p[2]//span[contains(@class, "postal-code")]//text()').getall()
+        date = sel1.xpath('//li/div[4]//text()').getall()
+        ret.append({"img": img,
+                    "title": title,
+                    "description": description,
+                    "price": price,
+                    "zipCode": postalCode,
+                    "date": date,
+                    })
+
+    # session = HTMLSession()
+    # result_page = session.post(url, data=payload)
+    #
+    # for row in result_page.html.xpath('//*[@id="ResultListData"]/ul/li'):
+    #     ret.append(row.getall())
+
+    return ret
+
